abae123f6131f95f3876519c6daa54e63507bd54
egerong <egert@isiro.ee>
2022-04-04 13:43:19 +0300

    Update config struct

--- main.cpp
+++ main.cpp
@@ -1,29 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <iostream>
+#include <sstream>
 #include <vector>
+#include <time.h>
 
 #include "transformer.h"
 
 int main(int argc, const char* argv[]) {
-    std::string text = u8"Selle hääle tegi C++";
+    char logLevel[] = "TF_CPP_MIN_LOG_LEVEL=3";
+    char cudaDevices[] = "CUDA_VISIBLE_DEVICES=0";
+    putenv(cudaDevices);
+    putenv(logLevel);
+    torch::set_num_threads(1);
+    //std::string text = u8"Katsetame kõnesünteeesi kiirust.";
 
     TransformerConfig config = {
         .verbose = true,
+        .cudaVisibleDevices = 0,
         .espeakLang = "Estonian",
         .espeakDataPath = "/usr/share/espeak-ng-data-1.50/",
         .modelPath = "/home/egert/EKI/TransformerTTS/out/model",
-        .vocoderPath = "/home/egert/EKI/TransformerTTS/out/hifigan",
-        .withStress = false,
         .sampleRate = 22050,
-        .nFFT = 1024,
         .nMel = 80,
-        .hopLength = 256,
-        .winLength = 1024,
-        .fMin = 0,
-        .fMax = 8000
+        .tfLogLevel = 3,
+        .vocoderPath = "/home/egert/EKI/TransformerTTS/out/hifigan",
     };
+    auto startTime = clock();
     Transformer transformer(config);
-    transformer.Synthesize(text);
+    auto initTime = clock();
+    printf(
+        "\nInit time: %f\n",
+        (double)(initTime - startTime) / CLOCKS_PER_SEC
+    );
+    int i;
+    std::string text;
+    for (i = 0; i < 100; i++) {
+        std::ostringstream os;
+        os << i << "Katsetame kõnesünteeesi kiirust " << i  <<".";
+        text = os.str();
+        std::cout << text << std::endl;
+        transformer.Synthesize(text);
+    }
+
 
     Test();
     return 0;
